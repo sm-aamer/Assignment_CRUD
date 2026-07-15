575091011d9efa77586b584d1b55af642f329cb4
Syed Muhammad Aamer <smamirdabir@gmail.com>
2026-07-15 19:32:47 +0500

    alembic env file modify

--- alembic/env.py
+++ alembic/env.py
@@ -9,7 +9,7 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
-
+from app.core.config import Base
 from dotenv import load_dotenv
 import os
 
@@ -31,7 +31,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
